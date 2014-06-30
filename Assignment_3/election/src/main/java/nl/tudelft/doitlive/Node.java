d4308755c96606dbf7d89966345907f92b2c9b70
Widar91 <eddy.bertoluzzo@gmail.com>
2014-06-30 22:39:27 +0200

    final version

--- Assignment_3/election/src/main/java/nl/tudelft/doitlive/Node.java
+++ Assignment_3/election/src/main/java/nl/tudelft/doitlive/Node.java
@@ -2,12 +2,19 @@ package nl.tudelft.doitlive;
 
 import java.io.Serializable;
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 public interface Node extends Remote, Serializable {
 	
 	public boolean sendMessage(Message m) throws java.rmi.RemoteException;
 	
-	public boolean pulse(int r) throws java.rmi.RemoteException;
+	//public boolean pulse(int r) throws java.rmi.RemoteException;
+	
+	public void pulse1(int r) throws RemoteException;
+	
+	public void pulse2(int r) throws RemoteException;
+	
+	public void pulse3(int r) throws RemoteException;
 
 	public int getId() throws java.rmi.RemoteException; 
 	
--- Assignment_3/election/src/main/java/nl/tudelft/doitlive/NodeImpl.java
+++ Assignment_3/election/src/main/java/nl/tudelft/doitlive/NodeImpl.java
@@ -5,6 +5,7 @@ import java.rmi.server.UnicastRemoteObject;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
@@ -18,48 +19,48 @@ public class NodeImpl extends UnicastRemoteObject implements Node {
 	private int value;
 	private boolean decided = false;
 	private Random random; 
-	private List<Message> ms1;
-	private List<Message> ms2;
-	private List<Message> ms3;
+	private CopyOnWriteArrayList<Message> ms1;
+	private CopyOnWriteArrayList<Message> ms2;
 	private boolean isBetrayer;
 
 	public NodeImpl(int id, boolean isBetrayer) throws Throwable {
 		this.id = id;
 		this.random = new Random();
 		this.value = random.nextInt(2);
-		this.ms1 = new ArrayList<>();
-		this.ms2 = new ArrayList<>();
-		this.ms3 = new ArrayList<>();
+		this.ms1 = new CopyOnWriteArrayList<>();
+		this.ms2 = new CopyOnWriteArrayList<>();
 		this.isBetrayer = isBetrayer;
 		System.out.println("Instantiating node, id: " + id + " value: " + value);
 	}
 
+	public Integer delay = 0;
+	
 	@Override
-	public boolean pulse(int r) throws RemoteException {
-		
-		this.ms1 = new ArrayList<>();
-		this.ms2 = new ArrayList<>();
-		this.ms3 = new ArrayList<>();
-		
-		System.out.println("----- NOTIFICATION -----");
-		broadcastMessage(new Message(Message.N, r, value));
-		
-		while(ms1.size() < Config.nodes.length - Config.faultyNodes) {/*System.out.println("[id] " + id + " [messages size] " + messages.size());*/}
-		
-		List<Message> ms = new ArrayList<>();
-		synchronized(ms1) {
-			for(Message m : ms1) {
-				ms.add(m);
+	public void pulse1(int r) throws RemoteException {
+		new Thread(() -> {
+			
+			try {
+				Thread.sleep(1000+random.nextInt(2000));
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
-		}
-		
-		System.out.println(id + "> [messages] " + ms);
-//		int num0 = (int) messages.stream().filter(m -> m.value == 0).count();
-//		int num1 = (int) messages.stream().filter(m -> m.value == 1).count();
-		
+			
+			this.ms1 = new CopyOnWriteArrayList<>();
+			
+			System.out.println("----- NOTIFICATION -----");
+			broadcastMessage(new Message(Message.N, r, value));
+			
+			while(ms1.size() < Config.nodes.length - Config.faultyNodes) {/*System.out.println("[id] " + id + " [messages size] " + messages.size());*/}
+		}).start();
+		//return true;
+	}
+	
+	@Override
+	public void pulse2(int r) throws RemoteException {
+		new Thread(() -> { 
 		int num0 = 0;
 		int num1 = 0;
-		for(Message m : ms) {
+		for(Message m : ms1) {
 			if (m.value == 0) {
 				num0++;
 			} else if (m.value == 1) {
@@ -77,24 +78,17 @@ public class NodeImpl extends UnicastRemoteObject implements Node {
 			broadcastMessage(new Message(Message.P, r, -13));
 		}
 		
-		if(decided)
-			return true;
-		
 		while(ms2.size() < Config.nodes.length - Config.faultyNodes) {System.out.println("[id] " + id + " [messages size] " + ms2.size());}
 		
-		List<Message> msp = new ArrayList<>();
-		synchronized(ms2) {
-			for(Message m : ms2) {
-				msp.add(m);
-			}
-		}
-		
-//		num0 = (int) messages.stream().filter(m -> m.value == 0).count();
-//		num1 = (int) messages.stream().filter(m -> m.value == 1).count();
-		
-		num0 = 0;
-		num1 = 0;
-		for(Message m : msp) {
+		}).start();
+	}
+	
+	@Override
+	public void pulse3(int r) throws RemoteException {
+		new Thread(() -> {
+		int num0 = 0;
+		int num1 = 0;
+		for(Message m : ms2) {
 			if (m.value == 0) {
 				num0++;
 			} else if (m.value == 1) {
@@ -102,8 +96,8 @@ public class NodeImpl extends UnicastRemoteObject implements Node {
 			}
 		}
 		
-		c = Math.max(num0, num1);
-		w = c == num0 ? 0 : 1;
+		int c = Math.max(num0, num1);
+		int w = c == num0 ? 0 : 1;
 
 		System.out.println("----- DECISION -----");
 		
@@ -114,21 +108,23 @@ public class NodeImpl extends UnicastRemoteObject implements Node {
 				decided = true;
 				System.out.println("I HAVE DECIDED FELLAS: " + decision);
 			}
-		} 
+		}
 		else if (!decided) {
 			value = random.nextInt(2);
 		}
 		
-		return decided;
+		}).start();
+		
+		//return decided;
 	}
 	
 	@Override
-	public synchronized boolean sendMessage(Message m) throws RemoteException {
+	public boolean sendMessage(Message m) throws RemoteException {
 		if(m.type == Message.N) {
-			ms1.add(m);
+			synchronized(ms1) { ms1.add(m); }
 		}
 		else if (m.type == Message.P) {
-			ms2.add(m);
+			synchronized(ms2) { ms2.add(m); }
 		}
 		System.out.println("message: " + m);
 		return m != null;
@@ -149,14 +145,13 @@ public class NodeImpl extends UnicastRemoteObject implements Node {
 		int numSent = 0;
 		if (isBetrayer) {
 			System.out.println("I AM A BETRAYERRRR: " + id);
-			msg.value = 0;
-			//return;
+			msg.value = random.nextInt(2);
+			if (random.nextInt(2) == 0)
+				return;
 		}
 		
 		for (String receiverURL : Config.nodes) {
-			//String[] parts = receiverURL.split("/");
-			//int receiverId = Integer.parseInt(parts[parts.length-1]); 
-			//if(receiverId != id) {
+			
 				try {
 					System.out.println(Thread.currentThread().getId()+ "-" + id + ">> Sending Message to " + receiverURL + ": " + msg.toString());
 
@@ -169,7 +164,7 @@ public class NodeImpl extends UnicastRemoteObject implements Node {
 				} catch (Exception e) { 
 					e.printStackTrace(); 
 				}
-			//}
+
 		}
 		
 		System.out.println("**** NUM SENT:" + numSent);
